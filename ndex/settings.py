44413fbda79de7ba9dd5adf4db298aeac849bc77
Michael Snider <msilvprog7@gmail.com>
2014-04-01 20:01:53 -0400

    Adjustments for heroku upload

--- ndex/settings.py
+++ ndex/settings.py
@@ -75,6 +75,34 @@ DATABASES = {
     }
 }
 
+
+
+IS_HEROKU = False
+
+if IS_HEROKU:
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    import os
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
